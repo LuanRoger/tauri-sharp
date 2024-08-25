e465313faf653379f94f080c2d45ad2c23050e26
ROG <luan.roger.2003@gmail.com>
2024-08-25 20:42:55 -0300

    feat: create custom compile options and support to multiple proto

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,8 +1,27 @@
-use std::error::Error;
+use std::{error::Error, path::Path};
 
 fn main() -> Result<(), Box<dyn Error>> {
     tauri_build::build();
-    tonic_build::compile_protos("protos/greet.proto")?;
+
+    compile_protos(&["../protos/greet.proto"])?;
+
+    Ok(())
+}
+
+fn compile_protos(protos: &[impl AsRef<Path>]) -> Result<(), Box<dyn Error>> {
+    if protos.is_empty() {
+        return Err("No proto files found".into());
+    }
+
+    let proto_path: &Path = protos[0].as_ref();
+    let proto_dir = proto_path
+        .parent()
+        .expect("proto file should reside in a directory");
+
+    tonic_build::configure()
+        .build_server(false)
+        .build_client(true)
+        .compile(&[proto_path], &[proto_dir])?;
 
     Ok(())
 }
